5225cd064e218960bf4ccdb2ec57270e8a7f0cc8
ygqygq2 <ygqygq2@qq.com>
2023-01-05 17:47:47 +0800

    chore: 测试 git-crypt
    Signed-off-by: ygqygq2 <ygqygq2@qq.com>

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
+import dotenv from 'dotenv';
+import Joi from 'joi';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath,
+      load: [() => dotenv.config({ path: '.env' })],
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string().valid('development', 'production', 'test').default('development'),
+        DB_HOST: Joi.string().default('localhost'),
+        DB_PORT: Joi.number().default(3306),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/enum/config.enum.ts
@@ -0,0 +1,5 @@
+export enum ConfigEnum {
+  DB_HOST = 'DB_HOST',
+  DB_USERNAME = 'DB_USERNAME',
+  DB_PASSWORD = 'DB_PASSWORD',
+}
