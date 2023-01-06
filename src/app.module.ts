a1a59a5b3175419aaf6ad9f99e7278f1664342c8
ygqygq2 <ygqygq2@qq.com>
2023-01-06 18:27:18 +0800

    feat: 增加 typeorm
    Signed-off-by: ygqygq2 <ygqygq2@qq.com>

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,20 +3,58 @@ import Joi from 'joi';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import {ConfigModule, ConfigService} from '@nestjs/config';
+import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigEnum } from './enum/config.enum';
+
+const envFilePath = `.env.${process.env.NODE_ENV || 'development'}`;
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath,
-      load: [() => dotenv.config({ path: '.env' })],
+      load: [() => dotenv.config({ path: '.env' })],  // 合并公共配置
       validationSchema: Joi.object({
         NODE_ENV: Joi.string().valid('development', 'production', 'test').default('development'),
         DB_HOST: Joi.string().default('localhost'),
         DB_PORT: Joi.number().default(3306),
+        DB_USERNAME: Joi.string(),
+        DB_PASSWORD: Joi.string(),
+        DB_DATABASE: Joi.string().required(),
+        DB_TYPE: Joi.string().valid('mysql', 'postgres').default('mysql'),
+        DB_SYNC: Joi.boolean().default(false),
       }),
     }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) =>
+        ({
+          type: configService.get(ConfigEnum.DB_TYPE),
+          host: configService.get(ConfigEnum.DB_HOST),
+          port: configService.get(ConfigEnum.DB_PORT),
+          username: configService.get(ConfigEnum.DB_USERNAME),
+          password: configService.get(ConfigEnum.DB_PASSWORD),
+          database: configService.get(ConfigEnum.DB_DATABASE),
+          entities: [],
+          synchronize: true,
+          logging: ['warn', 'error'],
+        } as TextDecoderOptions),
+    }),
+    // TypeOrmModule.forRoot({
+    //   type: 'mysql',
+    //   host: process.env.DB_HOST,
+    //   port: process.env.DB_PORT,
+    //   username: process.env.DB_USERNAME,
+    //   password: process.env.DB_PASSWORD,
+    //   database: process.env.DB_DATABASE,
+    //   entities: [],
+    //   synchronize: true,
+    //   logging: ['warn', 'error'],
+    // }),
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/enum/config.enum.ts
+++ src/enum/config.enum.ts
@@ -1,5 +1,8 @@
 export enum ConfigEnum {
+  DB_TYPE = 'DB_TYPE',
   DB_HOST = 'DB_HOST',
   DB_USERNAME = 'DB_USERNAME',
   DB_PASSWORD = 'DB_PASSWORD',
+  DB_DATABASE = 'DB_DATABASE',
+  DB_PORT = 'DB_PORT',
 }
