f58f1579197e1635c9c79b38a9da2b7775e5e2fd
ygqygq2 <ygqygq2@qq.com>
2023-01-13 18:13:59 +0800

    feat: 增加 user 接口示例
    Co-authored-by: ygqygq2 <29ygq@sina.com>

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
   // 指定url前缀
-  app.setGlobalPrefix('api');
+  app.setGlobalPrefix('api/v1');
   // 允许跨域
   app.enableCors();
   await app.listen(3000, '0.0.0.0');
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,18 +1,34 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Delete, Get, Patch, Post } from '@nestjs/common';
 
+import { ConfigService } from '@nestjs/config';
+
+import { User } from './user.entity';
 import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
-  constructor(private userService: UserService) {}
+  constructor(private userService: UserService, protected configService: ConfigService) {}
 
   @Get()
   getUsers(): any {
-    return this.userService.getUsers();
+    return this.userService.findAll();
+    // return this.userService.getUsers();
   }
 
   @Post()
   addUser(): any {
-    return this.userService.addUser();
+    const user = { username: 'ygqygq2', password: '123456' } as User;
+    return this.userService.create(user);
+    // return this.userService.addUser();
+  }
+
+  @Patch()
+  updateUser(): any {
+    return this.userService.update(1, { username: 'ygqygq2', password: 'ygqygq2' });
+  }
+
+  @Delete()
+  removeUser(): any {
+    return this.userService.remove(1);
   }
 }
