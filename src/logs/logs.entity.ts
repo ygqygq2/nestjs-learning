a96ee5e0b68e7eabaeb2c6620fd37c9b75ce3e0d
ygqygq2 <29ygq@sina.com>
2023-01-15 17:16:43 +0800

    chore: 加入 logs 目录到代码管理
    Signed-off-by: ygqygq2 <29ygq@sina.com>

---
+++ src/logs/logs.entity.ts
@@ -0,0 +1,24 @@
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+
+import { User } from '@/user/user.entity';
+
+@Entity()
+export class Logs {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  path: string;
+
+  @Column()
+  method: string;
+
+  @Column()
+  data: string;
+
+  @Column()
+  result: number;
+
+  @ManyToOne(() => User, (user) => user.logs)
+  user: User;
+}
